1a66c585d08b1b8cdf112c7a3dfaf6d1d4028884
Chris Chrome <christophercookman@gmail.com>
2021-08-02 12:00:53 -0400

    Send player count

--- script.lua
+++ script.lua
@@ -6,6 +6,7 @@ function onTick()
 	tick = tick + 1
 	if tick % 30 == 0 then
 		server.httpGet(chatbot_port, "/getmsgs")
+		server.httpGet(chatbot_port, "/setplayers?p="..#server.getPlayers())
 	end
 end
 
